c6dbb87fdd0b20d2c3abb317d756768d67e119d0
dariusrafei <dariusrafei@gmail.com>
2017-10-23 19:48:01 -0400

    homework submission

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+var friends = [
+
+    {
+        "name": "Jason",
+        "photos": "http://www.syfy.com/sites/syfy/files/wire/legacy/Jason-Voorhees-Friday-the-13th.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }, 
+    {
+        "name": "Freddie",
+        "photos": "https://vignette2.wikia.nocookie.net/epicrapbattlesofhistory/images/a/a0/Freddie_Mercury_Based_On.jpg/revision/latest?cb=20150823031623",
+        "scores":[
+            2,
+            1,
+            3,
+            4,
+            5,
+            2,
+            2,
+            3,
+            5,
+            2
+        ]
+    },
+    {
+        "name": "Stevie",
+        "photos": "https://cps-static.rovicorp.com/3/JPG_400/MI0001/399/MI0001399374.jpg?partner=allrovi.com",
+        "scores":[
+            3,
+            4,
+            6,
+            2,
+            5,
+            6,
+            8,
+            1,
+            9,
+            1
+        ]
+    },
+    {
+        "name": "Buddy Guy",
+        "photos": "hhttps://www.morrisonhotelgallery.com/images/big/ArtShay-MHG-Guy.jpg",
+        "scores":[
+            2,
+            7,
+            4,
+            6,
+            7,
+            2,
+            9,
+            9,
+            1,
+            4
+        ]
+    }
+];
+
+module.exports = friends;
